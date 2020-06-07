e7b77f8bf8f579c844b4b22cd9b9fa80b1b9aa2e
Phelipe <phelipe.omena58@gmail.com>
2020-06-07 13:20:09 -0300

    Aplicação Web - NLW - Ecoleta

---
+++ src/server.js
@@ -0,0 +1,106 @@
+//Criando um servidor
+const express = require("express")
+const server = express()
+
+//pegar o banco 
+const db = require("./database/db")
+
+//configurar pasta pública
+server.use(express.static("public"))
+
+//habilitar o uso do req.body da aplicação
+server.use(express.urlencoded({extended: true}))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//Configurar os caminhos da minha aplicação
+//pagina inicial
+//req:Requisição
+//res:Resposta
+server.get("/", (req, res) => {
+   return res.render("index.html", {title: "Um título"}) 
+})
+server.get("/create-point", (req, res) => {
+
+    //rec.query: query string da url
+    console.log(req.body)
+    
+    return res.render("create-point.html")
+})
+
+
+server.post("/savepoint", (req, res) =>{
+
+    //req.body: O corpo do nosso formulário
+    //console.log(req, res)
+
+
+    //inserir dados no banco de dados
+
+    const query =  `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values =  [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items ]
+
+    function afterInsertData(err){
+        if(err){
+         console.log(err)
+         return res.send("Erro no cadatro!")
+        }
+        
+        console.log("Cadastrado com sucesso!!!")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true})
+    }
+        
+   db.run(query, values, afterInsertData)
+    
+})
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+
+    if(search == ""){
+        //pesquisa vazia
+        return res.render("search-results.html", { total: 0})
+    }
+
+
+    //pegar os dados do banco de dados
+
+      db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+        if(err){
+            return console.log(err)
+        }
+        
+        const total = rows.length
+
+        //mostrar a pagina html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total})
+    })
+})
+
+//Ligando o servidor 
+server.listen(3000)
